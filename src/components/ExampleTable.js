03856958d05918bae12986014c4a9b4804b9c708
WIllsnSmith <silverflash150@gmail.com>
2021-07-15 15:12:47 +0300

    added

--- src/components/ExampleTable.js
+++ src/components/ExampleTable.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Example from "./Example";
+import ResultMessage from "./ResultMessage";
 
 const ExampleTable = (props) => {
   const [example, setExample] = useState(props.array);
+  const [result, setResult] = useState(false);
+  const [arrayResult, setArrayResult] = useState([]);
+  const [arrayCallback, setArrayCallback] = useState([]);
+  const [userAnswer, setUserAnswer] = useState([])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getAnswerArray();
+    let match = arrayResult.find(item => item.result === userAnswer);
+    arrayCallback.push(match);
+  };
+
+  const resultCount = (answer) => {
+    userAnswer.push(answer);
+  };
+
+  const getAnswerArray = () => {
+   arrayResult.push(example.result)
+  }
+
   return (
     <div>
-    <form>
-      {example.map((elem, index) => (
-        <ul key={index}>
-          <Example example={elem} />
-        </ul>
-      ))}
-      <button type="submit">Send result</button>
+      <form onSubmit={handleSubmit}>
+        {example.map((elem, index) => (
+          <ul key={index}>
+            <Example example={elem} resultCount={resultCount} />
+          </ul>
+        ))}
+        <button type="submit" onSubmit={() => handleSubmit}>Send result</button>
       </form>
     </div>
   );
---
+++ src/components/ResultMessage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+const ResultMessage = (props) => {
+
+    return <div>Ваш результат: {props.result}/10 </div>
+}
+
+export default ResultMessage;
