a3f18a4999f28d1f35858984ee9fa36c9cb588ff
WIllsnSmith <silverflash150@gmail.com>
2021-07-17 14:10:31 +0300

    add css for button

--- src/components/ResultMessage.js
+++ src/components/ResultMessage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "../styles/resultMessage.css";
 
-const ResultMessage = ({result, all, name}) => {
-    return <div>{name ? name : `Пользователь`}, твой результат: {result}/{all}. Молодец! </div>
-}
+const ResultMessage = ({ result, all, name }) => {
+  const showResult = () => {
+    return `${
+      name ? name : `Пользователь`
+    }, твой результат: ${result}/${all}. Молодец!`;
+  };
+  const refreshPage = () => {
+    window.location.reload(false);
+  }
+  return <div className="result">{showResult()} <button onClick={refreshPage} className="result_button">Попробовать снова</button></div>;
+};
 
 export default ResultMessage;
