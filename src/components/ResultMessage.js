86e93d8082fca4a5a11d3dd8c9c6c41e59a3d845
WIllsnSmith <silverflash150@gmail.com>
2021-07-17 17:33:42 +0300

    add font

--- src/components/ResultMessage.js
+++ src/components/ResultMessage.js
@@ -4,13 +4,20 @@ import "../styles/resultMessage.css";
 const ResultMessage = ({ result, all, name }) => {
   const showResult = () => {
     return `${
-      name ? name : `Пользователь`
-    }, твой результат: ${result}/${all}. Молодец!`;
+      name ? name : `User`
+    }, your result: ${result}/${all}. Good job!`;
   };
   const refreshPage = () => {
     window.location.reload(false);
-  }
-  return <div className="result">{showResult()} <button onClick={refreshPage} className="result_button">Попробовать снова</button></div>;
+  };
+  return (
+    <div className="result">
+      {showResult()}
+      <button onClick={refreshPage} className="result_button">
+      Try again
+      </button>
+    </div>
+  );
 };
 
 export default ResultMessage;
