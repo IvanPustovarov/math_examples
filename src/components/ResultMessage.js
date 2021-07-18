745fa55acc563230f17ea6dac8a2996198841cd4
WIllsnSmith <silverflash150@gmail.com>
2021-07-18 14:43:36 +0300

    final message

--- src/components/ResultMessage.js
+++ src/components/ResultMessage.js
@@ -3,7 +3,7 @@ import "../styles/resultMessage.css";
 
 const ResultMessage = ({ result, all, name }) => {
   const showResult = () => {
-    return <div className="result_text">{name ? name : `User`}, your result: {result}/{all}. Good job!</div>;
+    return <div className="result_text">{name ? name : `User`}, your result: {result}/{all}. {result >= 5 ? "Good job!" : "Nice, but you can do it better"}</div>;
   };
   const refreshPage = () => {
     window.location.reload(false);
